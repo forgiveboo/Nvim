cfa0d1631125b2c7976db9ce32c6134a0d08795f
forgiveboo <wyx5051@gmail.com>
2025-03-27 12:09:33 +0800

    更改和添加快捷键

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,30 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jj", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>", { desc = "file save"})
+map({ "n", "i", "v" }, "<C-z>", "<cmd> undo <cr>", { desc = "history undo" })
+map({ "n", "i", "v" }, "<C-y>", "<cmd> redo <cr>", { desc = "history redo" })
+
+map({ "n", "i", "v", "t" }, "<A-v>", function()
+  require("nvchad.term").toggle { pos = "vsp", id = "vtoggleTerm" }
+end, { desc = "terminal toggle vertical term" })
+map({ "n", "i", "v", "t" }, "<A-h>", function()
+  require("nvchad.term").toggle { pos = "sp", id = "htoggleTerm" }
+end, { desc = "terminal toggle horizontal term" })
+map({ "n", "i", "v", "t" }, "<A-i>", function()
+  require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
+end, { desc = "terminal toggle floating term" })
+
+map("n", "gb", "<C-o>", { desc = "jump jump back" })
+map("n", "gh", vim.lsp.buf.hover, { desc = "LSP hover" }) -- 等同于 `K`
+map("n", "ge", vim.diagnostic.open_float, { desc = "LSP show diagnostics" }) -- 唤出诊断信息，这个没有默认快捷键，我推荐大家都映射一下
+map({ "n", "i", "v" }, "<A-.>", vim.lsp.buf.code_action, { desc = "LSP code action" }) -- 唤出代码操作（由于 Vim/Neovim 中对 `Ctrl-.` 的映射有些问题，这里用 Alt 替代）
+map({ "n", "i", "v" }, "<F2>", function()
+  if vim.bo.filetype == "NvimTree" then
+    -- 如果在文件浏览器中，调用 NvimTree 的重命名功能
+    require("nvim-tree.api").fs.rename()
+  else
+    require "nvchad.lsp.renamer"()
+  end
+end, { desc = "LSP rename" }) -- 重命名符号，等同于 NvChad 的快捷键 `<leader>ra`
+map({ "n", "i", "v" }, "<F12>", vim.lsp.buf.definition, { desc = "LSP rename" }) -- 跳转到定义，等同于 `gd`
