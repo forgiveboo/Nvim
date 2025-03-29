bd48fd23bbff263444f85478c4370bc8825f22d8
forgiveboo <wyx5051@gmail.com>
2025-03-29 12:19:36 +0800

    去掉copilot

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,8 +3,8 @@ return {
   { -- Override `nvim-cmp` plugin
     "hrsh7th/nvim-cmp",
     lazy = false,
-    opts = function()
-      return require "configs.cmp"
-    end,
+    -- opts = function()
+    --   return require "configs.cmp"
+    -- end,
   },
 }
